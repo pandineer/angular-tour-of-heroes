497d141993b28ba536f165248bced3c9bb577b3a
pandineer <pandineer@gmail.com>
2021-06-29 08:49:51 +0900

    get heroes using HttpClient

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Observable, of } from 'rxjs';
 
@@ -10,13 +11,14 @@ import { MessageService } from "./message.service";
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // Web APIのURL
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
@@ -25,5 +27,9 @@ export class HeroService {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
