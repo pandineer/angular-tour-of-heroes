fe7de67a6ac82cdf19cf1599278b67da4c7f18f6
pandineer <pandineer@gmail.com>
2021-06-29 09:09:38 +0900

    update hero

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -13,6 +13,9 @@ import { MessageService } from "./message.service";
 })
 export class HeroService {
   private heroesUrl = 'api/heroes'; // Web APIのURL
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
 
   constructor(
     private http: HttpClient,
@@ -32,6 +35,14 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: サーバー上でヒーローを更新 */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
