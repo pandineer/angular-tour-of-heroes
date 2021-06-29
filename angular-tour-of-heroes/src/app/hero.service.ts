86342e7c6388b23f3d7459cf8d948570a039d2d9
pandineer <pandineer@gmail.com>
2021-06-29 09:25:34 +0900

    delete hero

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -54,6 +54,16 @@ export class HeroService {
     );
   }
 
+  /** DELETE: サーバーからヒーローを削除 */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
